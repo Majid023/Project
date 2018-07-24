33da8351fe0bd123916e48331eed69c143d9a560
Majid Ansari <33059093+Majid023@users.noreply.github.com>
2018-07-24 08:37:27 +0530

    mongodb init

---
+++ icfai/api/controllers/NewExampleController.js
@@ -0,0 +1,11 @@
+/**
+ * NewExampleController
+ *
+ * @description :: Server-side logic for managing newexamples
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+	
+};
+
